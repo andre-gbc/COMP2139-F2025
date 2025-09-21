27960b04c2139b2e0311fc416059a3b3213d7dea
andre-gbc <101573321@georgebrown.ca>
2025-09-21 16:59:32 -0400

    Lab 2 Completed

---
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -0,0 +1,40 @@
+using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    // GET
+    {
+        var projects = new List<Project>()
+        {
+            new Project {ProjectId = 1, Name = "Project 1", Description = "First Project"}, 
+        };
+        return View(projects);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    // GET
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Project project)
+    // GET
+    {
+        return RedirectToAction("index");
+    }
+    [HttpGet]
+    public IActionResult Details(int id)
+    // GET
+    {
+        var project = new List<Project>()
+        {
+            new Project {ProjectId = 1, Name = "Project 1", Description = "First Project"}, 
+        };
+        return View(project);
+    }
+}
